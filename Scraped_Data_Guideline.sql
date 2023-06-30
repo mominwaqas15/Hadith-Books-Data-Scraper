ff2322e2b72f6ed579b6f0f08c4c1fa9266d8881
Momin Waqas <116455246+mominwaqas15@users.noreply.github.com>
2023-06-30 18:22:05 +0500

    Add files via upload

---
+++ Scraped_Data_Guideline.sql
@@ -0,0 +1,54 @@
+create database Scraped_Data
+
+-- printing tables
+
+Select * from The_Fourty_hadith_Titles;
+
+Select * from Hadith_Contents;
+
+Select * from Book_Chapter_Info;
+
+Select * from Book_Bukhari
+
+Select * from Book_muslim
+
+Select * from book_Nasai
+
+Select * from Book_Abu_Dawud
+
+Select * from Book_Darimi
+
+Select * from Book_Ibn_e_Majah
+
+Select * from Book_Ahmed
+
+Select * from Book_Malik
+
+Select * from Book_Tirmidhi
+
+-- dropping tables 
+
+drop table The_Fourty_hadith_Titles;
+
+drop table Hadith_Contents;
+
+drop table Book_Chapter_Info;
+
+drop table Book_Bukhari
+
+drop table Book_muslim
+
+drop table book_Nasai
+
+drop table Book_Abu_Dawud
+
+drop table Book_Darimi
+
+drop table Book_Ibn_e_Majah
+
+drop table Book_Ahmed
+
+drop table Book_Malik
+
+drop table Book_Tirmidhi
+
